1e43e210ea7da2c2bb149467cedb8fbdb2731d31
isrou <israa.asia@gmail.com>
2019-01-18 15:36:35 +0100

    exercice fini avec route dans logo

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -18,7 +18,7 @@
           <div class="py-1">
             <div class="row align-items-center">
               <div class="col-2">
-                <h2 class="mb-0 site-logo"><a href="index.html">Cross<strong>fits</strong></a></h2>
+                <h2 class="mb-0 site-logo"><a href={{route("index")}}>Cross<strong>fits</strong></a></h2>
               </div>
               <div class="col-10">
                 <nav class="site-navigation text-right" role="navigation">
